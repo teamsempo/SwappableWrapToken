0a890c40a0f86c5be662cd23d8d640fc216b752d
Nick Williams <enjeyw@gmail.com>
2019-12-01 22:19:51 +1100

    more tests

--- test/swappableWrapToken.js
+++ test/swappableWrapToken.js
@@ -5,8 +5,14 @@ const TestnetERC20 = artifacts.require("TestnetERC20");
 let erc20_a;
 let erc20_b;
 
-async function initSwappable() {
-    return await SwappableWrapToken.new('Test Swappable', 'TSWAP', 18, erc20_a.address);
+async function initSwappable(tokens = []) {
+    let swappable = await SwappableWrapToken.new('Test Swappable', 'TSWAP', 18, erc20_a.address);
+
+    for (let i = 0; i < tokens.length; i++) {
+        await tokens[i].approve(swappable.address, '1000000000000000000') //1e18
+    }
+
+    return swappable
 }
 
 contract('SwappableWrapToken', (accounts) => {
@@ -36,24 +42,45 @@ contract('SwappableWrapToken', (accounts) => {
     });
 
     it('verifies that an token can be wrapped once the swappable is approved', async () => {
-        let swappable = await initSwappable();
-
-        await erc20_a.approve(swappable.address, '1000000000000000000'); //1e18
+        let swappable = await initSwappable([erc20_a]);
         await swappable.wrap('100');
 
         assert.equal(await swappable.balanceOf(accounts[0]), '100');
+        assert.equal(await swappable.totalSupply(), '100');
+    });
+
+    it('verifies that a swappable token can be transferred', async () => {
+        let swappable = await initSwappable([erc20_a]);
+
+        await swappable.wrap('100');
+        await swappable.transfer(accounts[1], '10');
+
+        assert.equal(await swappable.balanceOf(accounts[0]), '90');
+        assert.equal(await swappable.balanceOf(accounts[1]), '10');
+    });
+
+    it('verifies that a transferred swappable is unwrapped correctly', async () => {
+        let swappable = await initSwappable([erc20_a]);
+
+        await swappable.wrap('100');
+        await swappable.transfer(accounts[1], '10');
+
+        await swappable.unwrap('10', {from: accounts[1]});
+
+        assert.equal(await swappable.totalSupply(), '90');
+        assert.equal(await swappable.balanceOf(accounts[0]), '90');
+        assert.equal(await swappable.balanceOf(accounts[1]), '0');
+        assert.equal(await erc20_a.balanceOf(accounts[1]), '10');
     });
 
     it('verifies that an token can be swapped by the owner', async () => {
-        let swappable = await initSwappable();
+        let swappable = await initSwappable([erc20_a, erc20_b]);
 
-        await erc20_a.approve(swappable.address, '1000000000000000000'); //1e18
         await swappable.wrap('100');
 
         let preswap_a_bal = await erc20_a.balanceOf(accounts[0]);
         let preswap_b_bal = await erc20_b.balanceOf(accounts[0]);
 
-        await erc20_b.approve(swappable.address, '1000000000000000000'); //1e18
         await swappable.swapWrap(erc20_b.address);
 
         let postswap_a_bal = await erc20_a.balanceOf(accounts[0]);
@@ -65,5 +92,73 @@ contract('SwappableWrapToken', (accounts) => {
 
     });
 
+    it('verifies that an token can be swapped by the owner', async () => {
+        let swappable = await initSwappable([erc20_a, erc20_b]);
+
+        await swappable.wrap('100');
+
+        let preswap_a_bal = await erc20_a.balanceOf(accounts[0]);
+        let preswap_b_bal = await erc20_b.balanceOf(accounts[0]);
+
+        await swappable.swapWrap(erc20_b.address);
+
+        let postswap_a_bal = await erc20_a.balanceOf(accounts[0]);
+        let postswap_b_bal = await erc20_b.balanceOf(accounts[0]);
+
+        assert.equal(postswap_a_bal - preswap_a_bal, 100);
+        assert.equal(postswap_b_bal - preswap_b_bal, -100);
+        assert.equal(await swappable.wrappedToken(), erc20_b.address);
+
+    });
+
+    it('should throw if an account that is not the owner tries to make a swap' , async () => {
+        let swappable = await initSwappable([erc20_a]);
+        await swappable.wrap('100');
+        await erc20_b.allocateTo(accounts[0], '1000');
+        await utils.catchRevert(swappable.swapWrap(erc20_b.address, {from: accounts[1]}));
+    });
+
+    it('verifies a swapped token unwraps correctly', async () => {
+        let swappable = await initSwappable([erc20_a, erc20_b]);
+
+        await swappable.wrap('100');
+        await swappable.transfer(accounts[1], '10');
+
+        await swappable.swapWrap(erc20_b.address);
+
+        assert.equal(await erc20_b.balanceOf(accounts[1]), '0');
+
+        await swappable.unwrap('10', {from: accounts[1]});
+        assert.equal(await erc20_b.balanceOf(accounts[1]), '10');
+    });
+
+    it('verifies that swappable ownership can be transferred', async () => {
+        let swappable = await initSwappable([erc20_a, erc20_b]);
+
+        await swappable.wrap('100');
+        await swappable.transfer(accounts[1], '10');
+
+        await swappable.transferOwnership(accounts[1]);
+        await swappable.transferOwnership(accounts[2], {from: accounts[1]});
+
+        await erc20_b.allocateTo(accounts[2], '100');
+        await erc20_b.approve(swappable.address, '1000000000000000000', {from: accounts[2]});
+        await swappable.swapWrap(erc20_b.address, {from: accounts[2]});
+
+        assert.equal(await swappable.owner(), accounts[2]);
+    });
+
+
+    it('should throw if owner does not have enough balance to unwrap', async () => {
+        let swappable = await initSwappable([erc20_a, erc20_b]);
+
+        await swappable.wrap('100');
+        await swappable.transfer(accounts[1], '10');
+
+        await swappable.transferOwnership(accounts[1]);
+
+        await erc20_b.approve(swappable.address, '1000000000000000000', {from: accounts[1]});
+        await utils.catchRevert(swappable.swapWrap(erc20_b.address, {from: accounts[1]}));
+    });
 
 });
